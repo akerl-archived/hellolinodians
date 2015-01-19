5c8c319108851cd2ce683543de1ba2bd97850ecf
Les Aker <me@lesaker.org>
2015-01-19 00:17:07 -0500

    initial thoughts about twitter

--- lib/hellolinodians/alert.rb
+++ lib/hellolinodians/alert.rb
@@ -1,3 +1,5 @@
+require 'twitter'
+
 module HelloLinodians
   ##
   # Send alerts for messages
@@ -8,6 +10,22 @@ module HelloLinodians
 
     def run!
       p @message
+    end
+
+    private
+
+    def client
+      @client ||= Twitter::REST::Client.new do |config|
+        config.consumer_key = env('CONSUMER_KEY')
+        config.consumer_secret = env('CONSUMER_SECRET')
+        config.access_token = env('ACCESS_TOKEN')
+        config.access_token_secret = env('ACCESS_SECRET')
+      end
+    end
+
+    def env(name)
+      name = "TWITTER_#{name}"
+      ENV[name] || `heroku config:get #{name}`.strip
     end
   end
 end
